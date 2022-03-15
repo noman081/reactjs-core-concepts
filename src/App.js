b0d66355b5f47f5e353546152deb1da9aa375c85
noman081 <abdullahalnoman642@gmail.com>
2022-03-16 01:05:42 +0600

    After adding components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 
+const name = 'Noman';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Welcome to <span className='title'>My react App</span></h1>
+        <h1>{name}, Welcome to <span className='title'>My react App</span></h1>
+        <p className='para'>This is cool!</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -18,9 +20,35 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+
+      {/* new component add here */}
+      <Person></Person>
+      <Person></Person>
+      <Person></Person>
+      <h3>My New friend</h3>
+      <Friend></Friend>
+      <Friend></Friend>
     </div>
   );
+}
+
+const Person = () => {
+  return (
+    <div className='person'>
+      <h2>Name: Shakib Al Hasan</h2>
+      <p>Profession: Cricketer</p>
+    </div>
+  )
+}
+
+function Friend() {
+  return (
+    <div className='friend'>
+      <h2>Name: Sharif Osman</h2>
+      <p>Profession: Romeo</p>
+    </div>
+  )
 }
 
 export default App;
