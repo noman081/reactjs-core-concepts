9a01465c4ef31134223b1c129afe42763a2b7e69
noman081 <abdullahalnoman642@gmail.com>
2022-03-15 22:37:39 +0600

    after adding title

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Welcome to <span className='title'>My react App</span></h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
